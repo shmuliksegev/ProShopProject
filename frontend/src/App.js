ab5c4fbfc76955bf0ff2d5c4a7ba40f0a7c505a4
shmuliksegev <zzaaaazz6@gmail.com>
2020-10-13 14:35:50 +0300

    Create navbar and footer with react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
 
 const App = () => {
   return (
     <>
-      <h1>Welcom to pro shop</h1>
+      <Header />
+
+      <main className='py-3'>
+        <Container>
+          <h1>Welcom to pro shop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
