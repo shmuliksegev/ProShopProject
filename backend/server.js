398a7c9e0c12e8ab09cd68c53860069639cee9bd
shmuliksegev <zzaaaazz6@gmail.com>
2020-10-19 22:16:38 +0300

    Environment Variables (lesson 14)

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const products = require('./data/products')
+
+
+dotenv.config()
+
 
 //initialization 
 const app = express()
@@ -17,4 +22,6 @@ app.get('/api/products/:id',(req,res) => {
   })
 
 
-app.listen(5000,console.log('Server running on port 5000'))
+  const PORT = process.env.PORT || 5000
+
+app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
