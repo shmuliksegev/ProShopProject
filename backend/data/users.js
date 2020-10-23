19fcbe03cb012ca3a281be0b77f2981ffe5a5879
shmuliksegev <zzaaaazz6@gmail.com>
2020-10-23 12:26:50 +0300

    preparing sample data (lesson 20)

---
+++ backend/data/users.js
@@ -0,0 +1,23 @@
+import bcrypt from 'bcryptjs'
+
+
+const users = [
+    {
+        name:'Admin User',
+        email: 'admin@example.com',
+        password:bcrypt.hashSync('123456',10),
+        isAdmin:true
+    },
+    {
+        name:'Shmulik',
+        email: 'shmulik@example.com',
+        password:bcrypt.hashSync('123456',10),
+    },
+    {
+        name:'May',
+        email: 'may@example.com',
+        password:bcrypt.hashSync('123456',10),
+    }
+]
+
+export default users;
