1a13dbe91d34f7b05c4e635426ffb7d1025e417e
shmuliksegev <zzaaaazz6@gmail.com>
2020-10-25 12:22:06 +0200

    custom Error Handling (lesson 24)

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -19,7 +19,9 @@ router.get('/:id',expressAsyncHandler(async (req,res) => {
     if(product){
         res.json(product);
     }else {
-        res.status(404).json({message:'Product not found'})
+        // res.status(404).json({message:'Product not found'})
+        res.status(404)
+        throw new Error('Product Not Found')
     }
 }))
 
