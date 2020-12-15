79765ca4ed4b87b347f23c969308c03a51ad4812
shmuliksegev <zzaaaazz6@gmail.com>
2020-12-16 00:12:23 +0200

    user registreation & password encryption

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,5 +1,4 @@
-import bcrypt from 'bcryptjs';
-
+import bcrypt from 'bcryptjs'
 const users = [
   {
     name: 'Admin User',
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express';
 const router = express.Router();
 
-import { authUser,getUserProfile } from '../controllers/userControllers.js';
-import {protect} from '../middleware/authMiddleware.js'
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+} from '../controllers/userControllers.js';
+import { protect } from '../middleware/authMiddleware.js';
 
+router.route('/').post(registerUser);
 router.post('/login', authUser);
-router.route('/profile').get(protect,getUserProfile)
+router.route('/profile').get(protect, getUserProfile);
 
 export default router;
